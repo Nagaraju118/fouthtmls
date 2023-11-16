3397b6acbc4607098b8ddebb54500ea2967f34e6
Nagaraju118 <nagaraju312001@gmail.com>
2023-11-16 15:26:37 +0530

    first commit

---
+++ application3/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pag1(request):
+    return render(request,"pag1.html")
+
+def pag2(request):
+    return render(request,"pag2.html")
+
+def pag3(request):
+    return render(request,"pag3.html")
+
+def pag4(request):
+    return render(request,"pag4.html")
